3cb899fdef79a20b580a6a2f6d945ed98fbea738
iamraguls <raguls4477@gmail.com>
2026-01-20 12:37:56 +0530

    intial setup

---
+++ src/test/java/ragul/dev/ragul_spring_boot_starter_todo/RagulSpringBootStarterTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ragul.dev.ragul_spring_boot_starter_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagulSpringBootStarterTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
