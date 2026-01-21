6236e6daedf009a8d059197203eaf2e1b6efe646
iamraguls <raguls4477@gmail.com>
2026-01-21 13:13:27 +0530

    added auto-confiure and processor dependencies

--- src/main/java/dev/ragul/jps/JsonPlaceholderServiceConfiguration.java
+++ src/main/java/dev/ragul/jps/JsonPlaceholderServiceConfiguration.java
@@ -4,12 +4,14 @@ import dev.ragul.jps.todo.JpsTodoClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
 @AutoConfiguration
+@ConditionalOnClass(RestClient.class)
 @EnableConfigurationProperties(JsonPlaceholderServiceProperties.class)
 public class JsonPlaceholderServiceConfiguration {
 
